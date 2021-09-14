13c31f16c94530f656adfa7c9e7cae117f20dd12
@valentin_medvedev <v.medvedev@upp.com.ru>
2021-09-14 12:44:37 +0300

    фикс groupList. Профессии могут быть как объектом, так и массивом

--- src/app/components/groupList.jsx
+++ src/app/components/groupList.jsx
@@ -8,25 +8,43 @@ const GroupList = ({
     onItemSelect,
     selectedItem
 }) => {
-    const itemsData = !Array.isArray(items) ? Object.keys(items) : items;
-    // console.log(itemsData, selectedItem);
-    return (
-        <ul className="list-group">
-            {itemsData.map((item) => (
-                <li
-                    key={item[valueProperty]}
-                    className={
-                        "list-group-item" +
-                        (item === selectedItem ? " active" : "")
-                    }
-                    onClick={() => onItemSelect(item)}
-                    role="button"
-                >
-                    {item[contentProperty]}
-                </li>
-            ))}
-        </ul>
-    );
+    if (!Array.isArray(items)) {
+        return (
+            <ul className="list-group">
+                {Object.keys(items).map((item) => (
+                    <li
+                        key={items[item][valueProperty]}
+                        className={
+                            "list-group-item" +
+                            (items[item] === selectedItem ? " active" : "")
+                        }
+                        onClick={() => onItemSelect(items[item])}
+                        role="button"
+                    >
+                        {items[item][contentProperty]}
+                    </li>
+                ))}
+            </ul>
+        );
+    } else {
+        return (
+            <ul className="list-group">
+                {items.map((item) => (
+                    <li
+                        key={item[valueProperty]}
+                        className={
+                            "list-group-item" +
+                            (item === selectedItem ? " active" : "")
+                        }
+                        onClick={() => onItemSelect(item)}
+                        role="button"
+                    >
+                        {item[contentProperty]}
+                    </li>
+                ))}
+            </ul>
+        );
+    }
 };
 
 GroupList.defaultProps = {
